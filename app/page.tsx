ba0b8dbc7f87f037adb1a5f55d444a0faf521cdb
warutr <warutruangvatanasirikul@gmail.com>
2024-07-08 10:19:48 +0700

    add className of tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -11,22 +11,22 @@ const homePage = () => {
         colors="brightOrange"
         icon={sedansIcon}
         title="Sedans"
-        description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city 
-or on your next road trip."
+        description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
+        className="bg-brightOrange"
       />
       <CardSection
         colors="darkCyan"
         icon={suvsIcon}
         title="SUVs"
-        description="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation 
-and off-road adventures."
+        description="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
+        className="bg-darkCyan"
       />
       <CardSection
         colors="veryDarkCyan"
         icon={luxuryIcon}
         title="Luxury"
-        description="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury 
-rental and arrive in style."
+        description="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style."
+        className="bg-veryDarkCyan"
       />
     </div>
   );
